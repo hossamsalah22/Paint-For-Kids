af1716d373e16385375fb318fdc59e285bd7c60e
ZiadNady <94768692+ZiadNady@users.noreply.github.com>
2023-01-10 00:19:58 +0200

    final edit on first button

--- Actions/PickByFill.h
+++ Actions/PickByFill.h
@@ -2,13 +2,22 @@
 #define PICK_BY_FILL_H
 
 #include "./Action.h"
+#include "..\ApplicationManager.h"
+#include "..\GUI\GUI.h"
 
 class PickByFill : public Action
 {
+protected:
+	GUI* pGUI;
+	int FigAvailList[3];
+	int FigID;
+	int CorrectCounter;
+	CFigure* FigList[200];
+	int FigCount;
 public:
 	PickByFill(ApplicationManager* pApp);
 	void Execute();
-
+	void Select(Point P);
 };
 
 #endif
